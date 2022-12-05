7be5884267b04157e920e5946d452ae9fceddb5f
Ercan <96635891+KadirErcanKaradas@users.noreply.github.com>
2022-12-05 20:20:53 +0300

    first

---
+++ Assets/01_Script/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class GameManager : Singleton<GameManager>
+{
+    public event EventHandler<OnGameStageChangedEventArgs> OnGameStageChanged;
+    
+    public GameStage GameStage { get; private set; }
+
+    private void Start()
+    {
+        SetGameStage(GameStage.Loaded);
+    }
+
+    public void SetGameStage(GameStage gameStage)
+    {
+        GameStage = gameStage;
+
+        OnGameStageChanged?.Invoke(this, new OnGameStageChangedEventArgs { gameStage = gameStage });
+
+    }
+
+    public class OnGameStageChangedEventArgs : EventArgs
+    {
+        public GameStage gameStage;
+    }
+
+
+}
+
+public enum GameStage { NotLoaded, Loaded, Started, Win, Fail }
